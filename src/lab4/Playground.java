9ff58315ca6c7d9d875f020a01b370500e0ba244
celeste8 <celeste8@live.unc.edu>
2018-11-20 16:58:33 -0500

    Initial commit

---
+++ src/lab4/Playground.java
@@ -0,0 +1,19 @@
+package lab4;
+
+public class Playground {
+
+	// Making sure Charlie's new stuff works
+	public static void main(String[] args) {
+		Charlie charlie = new Charlie();
+		charlie.printPos();
+		charlie.buttonWait();
+		charlie.turnInPlaceRight(90);
+		charlie.printPos();
+		charlie.buttonWait();
+		charlie.turnInPlaceLeft(90);
+		charlie.printPos();
+		charlie.buttonWait();
+		
+//		charlie.spin();
+	}
+}
