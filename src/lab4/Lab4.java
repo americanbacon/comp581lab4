14aeebcf2bdbed9f80665f1b7fc757e2b5fe1312
celeste8 <celeste8@live.unc.edu>
2018-11-27 17:11:53 -0500

    Added charlie_work class
    Charlie_work works, but it needs some error correction done

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -3,7 +3,7 @@ package lab4;
 public class Lab4 {
 
 	public static void main(String[] args) {
-		Charlie charlie = new Charlie();
+		Charlie_Work charlie = new Charlie_Work();
 		System.out.println("Press button to start");
 		charlie.buttonWait();
 		charlie.bug2();
